824ce4960a16aabad518c29808b2f7198de6e5bd
yuqangy@qq.com <yuqangy@qq.com>
2018-02-24 23:34:14 +0800

    目录迁移，shaders和technique归纳为renderer

---
+++ Win32Project1/GE/renderer/shaders.cpp
@@ -0,0 +1,47 @@
+#include "stdafx.h"
+#include "shaders.h"
+
+GLuint loadShader(GLenum type, const char* shaderSrc)
+{
+	GLuint shader;
+	shader = glCreateShader(type);
+	if (0 == shader)
+		return 0;
+
+	//load shader sources
+	const GLchar* p[1];
+	p[0] = shaderSrc;
+	GLint L[1];
+	L[0] = strlen(shaderSrc);
+
+	glShaderSource(shader, 1, p, L);
+
+	glCompileShader(shader);
+
+	GLint compiled;
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
+	if (!compiled)
+	{
+		GLint infoLen = 0;
+		glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLen);
+		if (infoLen > 1)
+		{
+			char* infoLog = new char[sizeof(char)*infoLen];
+			glGetShaderInfoLog(shader, infoLen, NULL, infoLog);
+			printf("%s\r\n", infoLog);
+			//esLogMessage("error compile shader: %s\r\n", infoLog);
+			delete infoLog;
+		}
+		clearShader(shader);
+		return 0;
+	}
+	return shader;
+}
+
+void clearShader(GLuint s)
+{
+	if (s && 0 != s)
+	{
+		glDeleteShader(s);
+	}
+}
---
+++ Win32Project1/GE/renderer/technique.h
@@ -0,0 +1,37 @@
+#pragma once
+#include "shaders.h"
+#include <map>
+#include "Mat4.h"
+#include "Quaternion.h"
+
+
+class technique
+{
+public:
+	technique();
+	~technique();
+
+	virtual void init();
+	bool addShader(const char* vshFile = nullptr, const char* fshFile = nullptr, const char* gshFile = nullptr);
+	void enable();
+	void disable();
+
+	const uniformUnit* getUniforms(const char* name);
+	GLint getUniformLocation(const char* name);
+	GLint getUniformLocationEx(const char* name);
+
+	std::string getLastError();
+
+protected:
+	bool linkShader();
+	void initUniforms();
+
+protected:
+	GLuint m_shaderProg;
+	GLuint m_vshShader;
+	GLuint m_fshShader;
+	GLuint m_gshShader;
+	std::map<std::string, uniformUnit> m_userUniforms;
+
+};
+
